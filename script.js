39badf1e3725ea4f69968842425191f5e07de7e5
Matheus Sousa <dsousamt@gmail.com>
2022-06-20 13:18:54 -0300

    Create a playGame function

--- script.js
+++ script.js
@@ -6,36 +6,35 @@ const computerPlay = () => {
   return elements[randomNum];
 }
 
-const playerSelection = "Pedra".toLocaleLowerCase();
-const computerSelection = computerPlay().toLocaleLowerCase();
-
-// Pedra - Pedra, ----
-// Papel - Papel, -----
-// Tesoura - Tesoura ----
-
-// Pedra - Papel, 
-// Pedra - Tesoura
-// Papel - Tesoura
-
+let playerSelection = "Pedra".toLocaleLowerCase();
+let computerSelection = computerPlay().toLocaleLowerCase();
+let score = [0,0];
 
 const playRound = (playerSelection, computerSelection) => {
+  
   switch (playerSelection + computerSelection) {
     case "pedra"+"papel":
-      return "Você perdeu! Papel cobre pedra"
+      score[1] += 1;
+      return "Você perdeu! Papel cobre pedra";
       break;
     case "papel"+"pedra":
+      score[0] += 1;
       return "Você ganhou! Papel cobre pedra"
       break;
     case "pedra"+"tesoura":
+      score[0] += 1;
       return "Você ganhou! Pedra quebra tesoura"
       break;
     case "tesoura"+"pedra":
+      score[1] += 1;
       return "Você perdeu! Pedra quebra tesoura"
       break;
     case "papel"+"tesoura":
+      score[1] += 1;
       return "Você perdeu! Tesoura corta papel"
       break;
     case "tesoura"+"papel":
+      score[0] += 1;
       return "Você ganhou! Tesoura corta papel"
       break;
 
@@ -46,4 +45,17 @@ const playRound = (playerSelection, computerSelection) => {
   
 }
 
-console.log(playRound(playerSelection, computerSelection));
+const playGame = () => {
+  for (let index = 0; index < 5; index++) {
+    playerSelection = prompt("Qual a sua escolha? Pedra, Papel ou Tesoura?");
+    computerSelection = computerPlay().toLocaleLowerCase();
+    console.log(playRound(playerSelection, computerSelection));    
+  }
+  if (score[0] > score[1]) {
+    return console.log('Parabéns! Você ganhou a partida');
+  } else if (score[0] < score[1]) {
+    return console.log('Que pena! Você perdeu a partida');
+  } else {
+    return console.log('Ninguém ganhou, deu empate');
+  }
+}
